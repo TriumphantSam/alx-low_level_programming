54922c33c8a204a75f43cc3d1ca332ed6414c60d
TriumphantSam <triumphantsam30@gmail.com>
2022-03-11 23:24:25 +0100

    OOP

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,10 +13,10 @@ int main(void)
 		for (m = 49; m <= 57; m++)
 		{
 			if (m > n)
-
-			{																putchar(n);	
+			{											
+				putchar(n);	
 				putchar(m);
-				if (n != 56 || m != 57)							
+				if (n != 56 || m != 57)		
 				{
 					putchar(',');
 					putchar(' ');
