987dd36c5875796766440c7fb462a0e22157e290
TriumphantSam <triumphantsam30@gmail.com>
2022-03-15 23:22:04 +0100

    LJ

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- *  * print_to_98 - prints all natural numbers from n to 98,
- *   * followed by a new line
- *    * @n: print from this number
- *     */
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
 void print_to_98(int n)
 {
 	int i, j;
@@ -18,12 +18,12 @@ void print_to_98(int n)
 			printf("%d, ", i);
 		else if (i == 98)
 			printf("%d\n", i);
-		}	
+		}
 	} else if (n >= 98)
-	{       
+	{
 		for (j = n; j >= 98; j--)
 		{
-			if (j !=98)
+			if (j != 98)
 				printf("%d, ", j);
 	else if (j == 98)
 		printf("%d\n", j);
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,12 +11,12 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			k = j * 1;
+			k = j * i;
 			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
-			if (k < 10 && j !=0)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -33,5 +33,4 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
-}
-		
+}	
