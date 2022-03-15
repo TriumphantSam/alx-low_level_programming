a24133460c8e005d802ccb96c94ff998888df214
TriumphantSam <triumphantsam30@gmail.com>
2022-03-15 12:54:15 +0100

    ss

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	i = 0;
+
+	while (i<10)
+	{
+		alpha = 'a';
+
+		while (ch <= 'z;)
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+
+}
