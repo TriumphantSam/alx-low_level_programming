cb406074d501941a6ae3cbf3e2e7e105b1ca097e
TriumphantSam <triumphantsam30@gmail.com>
2022-03-21 22:29:34 +0100

    My commit

---
+++ 0x00-hello_world/hhhh.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+L
+int main(void)
+{
+	    return (0);
+}
+
+#!/bin/bash
+gcc $CFILE -S
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Raedme file for this project
