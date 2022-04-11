d7c226e2f43d6797cd536441ec9bb90ad6fff217
TriumphantSam <triumphantsam30@gmail.com>
2022-04-11 16:34:56 +0100

    All changes

--- practice/0-main.c
+++ practice/0-main.c
@@ -1,17 +1,11 @@
-#include <stdio.h>
-
 /**
- * main - using sizeof to dynamically determine the size of types char, int and float
+ * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+int main (void)
 {
-   int n; 
-
-   printf("Size of type 'char' on my computer: %lu bytes\n", sizeof(char));
-   printf("Size of type 'int' on my computer: %lu bytes\n", sizeof(int));
-   printf("Size of type 'float' on my computer: %lu bytes\n", sizeof(float));
-   printf("Size of type of my variable n on my computer: %lu bytes\n", sizeof(n));
-   return (0);
+	_puts_recursion("Puts with recursion");
+	return (0);
 }
+
